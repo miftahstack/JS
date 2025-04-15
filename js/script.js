860e8c4ed4d1e970ef9fb229515514650d621f96
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-04-15 12:39:37 +0600

    Added info

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Hello, Mates!");
+
+let name = "AR";
+let age = 33;
+let add = "Chittagong";
+let isSingle = true;
+
+let manush = {
+  name: "AR",
+  age: 33,
+  add: "Chittagong",
+  isSingle: true,
+};
