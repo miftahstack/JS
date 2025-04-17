004d7d796848013197bfbf813b7f3a3f926838d9
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-04-17 13:52:51 +0600

    Added kazi list

--- js/script.js
+++ js/script.js
@@ -23,3 +23,52 @@ console.log(num + " " + "X" + " " + 7 + " " + "=" + " " + (num*7))
 console.log(num + " " + "X" + " " + 8 + " " + "=" + " " + (num*8))
 console.log(num + " " + "X" + " " + 9 + " " + "=" + " " + (num*9))
 console.log(num + " " + "X" + " " + 10 + " " + "=" + " " + (num*10))
+
+// How a filter function works when we search for a specific value
+let gender = "Male";
+let age = 30;
+let salary = 100000;
+
+console.log("Kazi office lte-");
+if (gender == "Male") {
+  if (age >= 30) {
+    if (salary >= 100000) {
+      console.log("Ibe confirme partyy..Biyee kore felen");
+    } else {
+      console.log("Paisaa kam hae re baba");
+    }
+  } else {
+    console.log("Hanga no horis");
+  }
+} else {
+  console.log("Foya laigbo!");
+}
+
+// Marksheet
+
+let mark = 70
+
+if(mark < 0 || mark > 100){
+    console.log("কিরে কী সমস্যা, ভালো ঠিক ঠাক সংখ্যা দিতে পারেন না?");
+}
+else if(mark < 33){
+    console.log("স্বাগত, তুমি পটল তুলেছো!!!");
+}
+else if(mark >= 33 && mark <= 39){
+    console.log("You have got \"D\"");
+}
+else if(mark >= 40 && mark <= 49){
+    console.log("You have got \"C\"");
+}
+else if(mark >= 50 && mark <= 59){
+    console.log("You have got \"B\"");
+}
+else if(mark >= 60 && mark <= 69){
+    console.log("You have got \"A-\"");
+}
+else if(mark >= 70 && mark <= 79){
+    console.log("You have got \"A\"");
+}
+else if(mark >= 80 && mark <= 100){
+    console.log("You have got \"A+\"");
+}
